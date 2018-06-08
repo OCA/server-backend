6da5201b9fc94c36661d0ee831f4cbf1aa527dc4
Frédéric Garbely <frederic.garbely@brain-tec.ch>
2018-06-08 10:41:48 +0200

    [MIG] base_suspend_security: Migration to 11.0

--- base_suspend_security/models/base.py
+++ base_suspend_security/models/base.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
+
 from ..base_suspend_security import BaseSuspendSecurityUid
 
 
