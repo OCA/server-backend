90d2fa6769eb0c384d9217db36effae7a6ef0177
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2018-06-27 14:01:11 +0200

    [11.0][MIG] base_external_dbsource: Migration to v11.0

--- base_external_dbsource/__manifest__.py
+++ base_external_dbsource/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright <2011> <Daniel Reis, Maxime Chambreuil, Savoir-faire Linux>
 # Copyright <2016> <Henry Zhou MAXodoo>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'External Database Sources',
-    'version': '10.0.2.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'author': "Daniel Reis, "
               "LasLabs, "
               "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/server-tools',
+    'website': 'https://github.com/OCA/server-backend',
     'license': 'LGPL-3',
     'images': [
         'images/screenshot01.png',
