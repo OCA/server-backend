75a4a459ddbcb7f164cf67a2f1ef997300e7bf4c
elicoidal <eric.caudal@elico-corp.com>
2018-12-29 18:28:06 +0800

    Switch AGPL to LGPL license

--- base_user_role/__manifest__.py
+++ base_user_role/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright 2014 ABF OSIELL <http://osiell.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 
 {
     'name': 'User roles',
     'version': '12.0.1.0.0',
     'category': 'Tools',
     'author': 'ABF OSIELL, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'maintainers': ['ABF OSIELL', 'jcdrubay'],
+    'license': 'LGPL-3',
+    'maintainers': ['sebalix', 'jcdrubay', 'novawish'],
     'website': 'https://github.com/OCA/server-backend',
     'depends': [
         'base',
