6f667b97365cb245ae024ed897bacf19e35883a0
ernesto <ernesto.tejeda87@gmail.com>
2019-02-08 09:26:42 -0500

    [MIG] base_import_match: Migration to 12.0

--- base_import_match/__manifest__.py
+++ base_import_match/__manifest__.py
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Base Import Match",
     "summary": "Try to avoid duplicates before importing",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Tools",
     "website": "https://tecnativa.com",
-    "author": "Grupo ESOC Ingeniería de Servicios,"
-              "Tecnativa, "
+    "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
