ea309ffce4c75a8a89848cccdbe4fb28158c8fcc
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-08 10:40:38 +0100

    [MIG] base_external_dbsource_mssql: Migration to v12.0

--- base_external_dbsource_mssql/__manifest__.py
+++ base_external_dbsource_mssql/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     'name': 'External Database Source - MSSQL',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Tools',
     'author': "Daniel Reis, "
               "LasLabs, "
