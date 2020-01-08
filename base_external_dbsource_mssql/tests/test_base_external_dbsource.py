828ebece35e5a9db02d6b5d3c7c75b7832bf1bb2
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-08 10:40:38 +0100

    base_external_dbsource_mssql: Migration to v12.0
    [UPD] Update base_external_dbsource_mssql.pot

--- base_external_dbsource_mssql/tests/test_base_external_dbsource.py
+++ base_external_dbsource_mssql/tests/test_base_external_dbsource.py
@@ -4,18 +4,15 @@ import mock
 
 from odoo.tests import common
 
-
-ADAPTER = ('odoo.addons.base_external_dbsource_mssql.models'
-           '.base_external_dbsource.pymssql')
+ADAPTER = (
+    "odoo.addons.base_external_dbsource_mssql.models" ".base_external_dbsource.pymssql"
+)
 
 
 class TestBaseExternalDbsource(common.TransactionCase):
-
     def setUp(self):
         super(TestBaseExternalDbsource, self).setUp()
-        self.dbsource = self.env.ref(
-            'base_external_dbsource_mssql.demo_mssql',
-        )
+        self.dbsource = self.env.ref("base_external_dbsource_mssql.demo_mssql")
 
     def test_connection_close_mssql(self):
         """ It should close the connection """
@@ -26,16 +23,14 @@ class TestBaseExternalDbsource(common.TransactionCase):
     def test_connection_open_mssql(self):
         """ It should call SQLAlchemy open """
         with mock.patch.object(
-            self.dbsource, '_connection_open_sqlalchemy'
+            self.dbsource, "_connection_open_sqlalchemy"
         ) as parent_method:
             self.dbsource.connection_open_mssql()
             parent_method.assert_called_once_with()
 
     def test_excecute_mssql(self):
         """ It should pass args to SQLAlchemy execute """
-        expect = 'sqlquery', 'sqlparams', 'metadata'
-        with mock.patch.object(
-            self.dbsource, '_execute_sqlalchemy'
-        ) as parent_method:
+        expect = "sqlquery", "sqlparams", "metadata"
+        with mock.patch.object(self.dbsource, "_execute_sqlalchemy") as parent_method:
             self.dbsource.execute_mssql(*expect)
             parent_method.assert_called_once_with(*expect)
