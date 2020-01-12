d5feb450d163eb54a69335f49390a397fd441f1b
fkantelberg <florian.kantelberg@initos.com>
2020-01-12 22:16:13 +0100

    Mute radicale logger for testing

--- base_dav/radicale/collection.py
+++ base_dav/radicale/collection.py
@@ -130,7 +130,3 @@ class Collection(BaseCollection):
 
     def list(self):
         return self.collection.dav_list(self.path_components)
-
-    @classmethod
-    def create_collection(cls, href, collection=None, props=None):
-        return None
