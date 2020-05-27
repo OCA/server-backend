b94f61d1e227a79b20873b5af539e95e7f78bb95
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-05-27 20:01:25 +0200

    [MIG] base_global_discount: Migration to 12.0

--- base_global_discount/__manifest__.py
+++ base_global_discount/__manifest__.py
@@ -1,8 +1,9 @@
 # Copyright 2019 Tecnativa S.L. - David Vidal
+# Copyright 2020 Xtendoo - Manuel Calero
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Base Global Discount',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Base',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
