df7608d90bb00cdfddf3bc89116f61daf64fedd3
Ronald Portier <ronald@therp.nl>
2020-08-20 14:01:02 +0200

    [IMP] : black, isort, prettier

--- base_external_system/tests/test_external_system.py
+++ base_external_system/tests/test_external_system.py
@@ -7,59 +7,46 @@ from .common import Common
 
 
 class TestExternalSystem(Common):
-
     def setUp(self):
         super(TestExternalSystem, self).setUp()
-        self.record = self.env.ref('base_external_system.external_system_os')
+        self.record = self.env.ref("base_external_system.external_system_os")
 
     def test_get_system_types(self):
         """It should return at least the test record's interface."""
         self.assertIn(
             (self.record._name, self.record._description),
-            self.env['external.system']._get_system_types(),
+            self.env["external.system"]._get_system_types(),
         )
 
     def test_check_fingerprint_blank(self):
         """It should not allow blank fingerprints when checking enabled."""
         with self.assertRaises(ValidationError):
-            self.record.write({
-                'ignore_fingerprint': False,
-                'fingerprint': False,
-            })
+            self.record.write({"ignore_fingerprint": False, "fingerprint": False})
 
     def test_check_fingerprint_allowed(self):
         """It should not raise a validation error if there is a fingerprint."""
-        self.record.write({
-            'ignore_fingerprint': False,
-            'fingerprint': 'Data',
-        })
+        self.record.write({"ignore_fingerprint": False, "fingerprint": "Data"})
         self.assertTrue(True)
 
     def test_client(self):
         """It should yield the open interface client."""
-        with self._mock_method('client', self.record) as magic:
+        with self._mock_method("client", self.record) as magic:
             with self.record.system_id.client() as client:
                 self.assertEqual(client, magic().__enter__())
 
     def test_create_creates_and_assigns_interface(self):
         """It should create and assign the interface on record create."""
-        record = self.env['external.system'].create({
-            'name': 'Test',
-            'system_type': 'external.system.os',
-        })
+        record = self.env["external.system"].create(
+            {"name": "Test", "system_type": "external.system.os"}
+        )
         self.assertEqual(
-            record.interface._name, 'external.system.os',
+            record.interface._name, "external.system.os",
         )
 
     def test_create_context_override(self):
         """It should allow for interface create override with context."""
-        model = self.env['external.system'].with_context(
-            no_create_interface=True,
-        )
-        record = model.create({
-            'name': 'Test',
-            'system_type': 'external.system.os',
-        })
+        model = self.env["external.system"].with_context(no_create_interface=True,)
+        record = model.create({"name": "Test", "system_type": "external.system.os"})
         self.assertFalse(record.interface)
 
     def test_action_test_connection(self):
