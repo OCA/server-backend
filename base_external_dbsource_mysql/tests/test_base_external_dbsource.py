52e0147d3ecaf367ca7879d2409ed6f8f4233c3b
David Alonso (Solvos) <david.alonso@solvos.es>
2020-09-01 18:09:29 +0200

    [IMP] base_external_dbsource_mysql: black, isort, prettier

--- base_external_dbsource_mysql/tests/test_base_external_dbsource.py
+++ base_external_dbsource_mysql/tests/test_base_external_dbsource.py
@@ -4,18 +4,15 @@ import mock
 
 from odoo.tests import common
 
-
-ADAPTER = ('odoo.addons.base_external_dbsource_mysql.models'
-           '.base_external_dbsource.MySQLdb')
+ADAPTER = (
+    "odoo.addons.base_external_dbsource_mysql.models" ".base_external_dbsource.MySQLdb"
+)
 
 
 class TestBaseExternalDbsource(common.TransactionCase):
-
     def setUp(self):
         super(TestBaseExternalDbsource, self).setUp()
-        self.dbsource = self.env.ref(
-            'base_external_dbsource_mysql.demo_mysql',
-        )
+        self.dbsource = self.env.ref("base_external_dbsource_mysql.demo_mysql",)
 
     def test_connection_close_mysql(self):
         """ It should close the connection """
@@ -25,17 +22,13 @@ class TestBaseExternalDbsource(common.TransactionCase):
 
     def test_connection_open_mysql(self):
         """ It should call SQLAlchemy open """
-        with mock.patch.object(
-            self.dbsource, 'connection_open_mysql'
-        ) as parent_method:
+        with mock.patch.object(self.dbsource, "connection_open_mysql") as parent_method:
             self.dbsource.connection_open_mysql()
             parent_method.assert_called_once_with()
 
     def test_excecute_mysql(self):
         """ It should pass args to SQLAlchemy execute """
-        expect = 'sqlquery', 'sqlparams', 'metadata'
-        with mock.patch.object(
-            self.dbsource, 'execute_mysql'
-        ) as parent_method:
+        expect = "sqlquery", "sqlparams", "metadata"
+        with mock.patch.object(self.dbsource, "execute_mysql") as parent_method:
             self.dbsource.execute_mysql(*expect)
             parent_method.assert_called_once_with(*expect)
