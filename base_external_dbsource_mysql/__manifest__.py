47e8c42c6f0bef762af0cd447289314d402840d2
David Alonso (Solvos) <david.alonso@solvos.es>
2020-09-01 19:06:26 +0200

    [MIG] base_external_dbsource_mysql: Migration to 13.0

--- base_external_dbsource_mysql/__manifest__.py
+++ base_external_dbsource_mysql/__manifest__.py
@@ -3,13 +3,13 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "External Database Source - MySQL",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Tools",
-    "author": "Daniel Reis, " "LasLabs, " "Odoo Community Association (OCA)",
+    "author": "Daniel Reis, LasLabs, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-backend",
     "license": "LGPL-3",
-    "depends": ["base_external_dbsource",],
-    "external_dependencies": {"python": ["sqlalchemy", "MySQLdb",],},
-    "demo": ["demo/base_external_dbsource.xml",],
+    "depends": ["base_external_dbsource"],
+    "external_dependencies": {"python": ["sqlalchemy", "mysqlclient"]},
+    "demo": ["demo/base_external_dbsource.xml"],
     "installable": True,
 }
--- base_external_dbsource_mysql/models/base_external_dbsource.py
+++ base_external_dbsource_mysql/models/base_external_dbsource.py
@@ -2,34 +2,13 @@
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-import logging
+import sqlalchemy
 
 from odoo import models
 
-_logger = logging.getLogger(__name__)
+from odoo.addons.base_external_dbsource.models import base_external_dbsource
 
-try:
-    from odoo.addons.base_external_dbsource.models import base_external_dbsource
-
-    CONNECTORS = base_external_dbsource.BaseExternalDbsource.CONNECTORS
-    try:
-        import MySQLdb
-
-        CONNECTORS.append(("mysql", "MySQL"))
-        assert MySQLdb
-    except (ImportError, AssertionError):
-        _logger.info(
-            'MySQLdb not available. Please install "mysqlclient" ' "python package."
-        )
-    try:
-        import sqlalchemy
-    except ImportError:
-        _logger.info(
-            "SQLAlchemy library not available. Please "
-            'install "sqlalchemy" python package.'
-        )
-except ImportError:
-    _logger.info("base_external_dbsource Odoo module not found.")
+base_external_dbsource.BaseExternalDbsource.CONNECTORS.append(("mysql", "MySQL"))
 
 
 class BaseExternalDbsource(models.Model):
