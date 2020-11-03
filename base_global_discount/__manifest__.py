8675915210ba75825e9c40ea2c9d9ed08f7029f7
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-11-03 11:05:41 -0500

    [IMP] base_global_discount: black, isort, prettier

--- base_global_discount/__manifest__.py
+++ base_global_discount/__manifest__.py
@@ -2,22 +2,19 @@
 # Copyright 2020 Xtendoo - Manuel Calero
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Base Global Discount',
-    'version': '12.0.1.0.0',
-    'category': 'Base',
-    'author': 'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/server-backend',
-    'license': 'AGPL-3',
-    'depends': [
-        'product',
+    "name": "Base Global Discount",
+    "version": "12.0.1.0.0",
+    "category": "Base",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-backend",
+    "license": "AGPL-3",
+    "depends": ["product",],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/security.xml",
+        "views/global_discount_views.xml",
+        "views/res_partner_views.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/security.xml',
-        'views/global_discount_views.xml',
-        'views/res_partner_views.xml',
-    ],
-    'application': False,
-    'installable': True,
+    "application": False,
+    "installable": True,
 }
