ccc8da205043a053cf5805afb1e71f2df4bc6e15
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-10-02 14:18:01 +0200

    [OU-FIX] base_global_discount: Fix SQL that splits discounts by type
    It needs to link the global discount record to see its type.

--- base_global_discount/migrations/13.0.1.0.0/post-migration.py
+++ base_global_discount/migrations/13.0.1.0.0/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Tecnativa - Ernesto Tejeda
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
@@ -10,27 +11,21 @@ def migrate(env, version):
         env.cr,
         """
         INSERT INTO customer_global_discount_rel
-            (partner_id, global_discount_id)
-            SELECT
-                partner_id,
-                global_discount_id
-            FROM
-                global_discount_res_partner_rel
-            WHERE
-                discount_scope = 'sale';
+        (partner_id, global_discount_id)
+        SELECT rel.partner_id, rel.global_discount_id
+        FROM global_discount_res_partner_rel rel
+        JOIN global_discount gd ON gd.id = rel.global_discount_id
+        WHERE gd.discount_scope = 'sale';
         """,
     )
     openupgrade.logged_query(
         env.cr,
         """
         INSERT INTO supplier_global_discount_rel
-            (partner_id, global_discount_id)
-            SELECT
-                partner_id,
-                field_id
-            FROM
-                global_discount_res_partner_rel
-            WHERE
-                discount_scope = 'purchase';
+        (partner_id, global_discount_id)
+        SELECT rel.partner_id, rel.global_discount_id
+        FROM global_discount_res_partner_rel rel
+        JOIN global_discount gd ON gd.id = rel.global_discount_id
+        WHERE gd.discount_scope = 'purchase';
         """,
     )
