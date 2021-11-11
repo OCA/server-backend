bcd732d3b8139f520b234cecaffaff8697e51e3c
Tatiana Deribina <tatiana.deribina@sprintit.fi>
2021-11-11 18:10:44 +0200

    [MIG] base_user_role: Migration to 15.0

--- base_user_role/__manifest__.py
+++ base_user_role/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "User roles",
-    "version": "14.0.2.1.1",
+    "version": "15.0.0.0.0",
     "category": "Tools",
     "author": "ABF OSIELL, Odoo Community Association (OCA)",
     "license": "LGPL-3",
