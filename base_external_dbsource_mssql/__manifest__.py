5db9fbeef5ca29498add80f4f10565231d8dd071
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-18 08:10:32 +0100

    [MIG] base_external_dbsource_mssql: Migration to 15.0
    TT38357

--- base_external_dbsource_mssql/__manifest__.py
+++ base_external_dbsource_mssql/__manifest__.py
@@ -3,13 +3,13 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "External Database Source - MSSQL",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Tools",
     "author": "Daniel Reis, " "LasLabs, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-backend",
     "license": "LGPL-3",
     "depends": ["base_external_dbsource_sqlite"],
-    "external_dependencies": {"python": ["sqlalchemy", "pymssql<=2.2.5"]},
+    "external_dependencies": {"python": ["pymssql<=2.2.5"]},
     "demo": ["demo/base_external_dbsource.xml"],
     "installable": True,
 }
