fb9c0670cb6e0541afae2c311e8f36410949c0fc
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-23 08:19:51 +0100

    [MIG] base_external_dbsource_mysql: Migration to 15.0
    TT37274

--- base_external_dbsource_mysql/tests/test_base_external_dbsource.py
+++ base_external_dbsource_mysql/tests/test_base_external_dbsource.py
@@ -11,7 +11,7 @@ ADAPTER = (
 
 class TestBaseExternalDbsource(common.TransactionCase):
     def setUp(self):
-        super(TestBaseExternalDbsource, self).setUp()
+        super().setUp()
         self.dbsource = self.env.ref("base_external_dbsource_mysql.demo_mysql")
 
     def test_connection_close_mysql(self):
@@ -22,13 +22,15 @@ class TestBaseExternalDbsource(common.TransactionCase):
 
     def test_connection_open_mysql(self):
         """It should call SQLAlchemy open"""
-        with mock.patch.object(self.dbsource, "connection_open_mysql") as parent_method:
+        with mock.patch.object(
+            type(self.dbsource), "connection_open_mysql"
+        ) as parent_method:
             self.dbsource.connection_open_mysql()
             parent_method.assert_called_once_with()
 
     def test_excecute_mysql(self):
         """It should pass args to SQLAlchemy execute"""
         expect = "sqlquery", "sqlparams", "metadata"
-        with mock.patch.object(self.dbsource, "execute_mysql") as parent_method:
+        with mock.patch.object(type(self.dbsource), "execute_mysql") as parent_method:
             self.dbsource.execute_mysql(*expect)
             parent_method.assert_called_once_with(*expect)
