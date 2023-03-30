bc8f25bed274b2c44c0110ac379c4af0f9068589
Andrea Cattalani <andrea@moduon.team>
2023-03-30 12:33:08 +0200

    [14.0][MIG]base_external_dbsource

--- base_external_dbsource/__manifest__.py
+++ base_external_dbsource/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "External Database Sources",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "author": "Daniel Reis, " "LasLabs, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-backend",
