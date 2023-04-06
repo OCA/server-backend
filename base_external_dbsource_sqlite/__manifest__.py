cc9264992182c859f2013ea5c496dc64e5c93095
Andrea Cattalani <andrea@moduon.team>
2023-04-06 13:37:42 +0200

    [MIG] base_external_dbsource_sqlite: migration to 16.0

--- base_external_dbsource_sqlite/__manifest__.py
+++ base_external_dbsource_sqlite/__manifest__.py
@@ -3,9 +3,10 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "External Database Source - SQLite",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "author": "Daniel Reis, LasLabs, Odoo Community Association (OCA)",
+    "maintainers": ["anddago78"],
     "website": "https://github.com/OCA/server-backend",
     "license": "LGPL-3",
     "depends": ["base_external_dbsource"],
