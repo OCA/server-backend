b03bb9fb8c9bca6f38227fcda95642dcc57d1c01
Holger Brunn <mail@hunki-enterprises.com>
2023-07-10 21:17:17 +0200

    [ADD] base_ical

---
+++ base_ical/models/res_users.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    ical_ids = fields.One2many("base.ical", compute="_compute_ical_ids")
+
+    def _compute_ical_ids(self):
+        for this in self:
+            this.ical_ids = self.env["base.ical"].search([])
