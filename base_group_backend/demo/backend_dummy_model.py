48a8d4eec141e23e4412e0315684e66c3a06a7ee
David Beal <david.beal@akretion.com>
2023-08-07 17:38:47 +0200

    FIX base_grp_backend: precommit and better names in ir.model.access

--- base_group_backend/demo/backend_dummy_model.py
+++ base_group_backend/demo/backend_dummy_model.py
@@ -1,11 +1,16 @@
 from odoo import fields, models
 
+
 class BackendDummyModel(models.Model):
     _name = "backend.dummy.model"
     _inherit = ["mail.activity.mixin"]
-    _description = 'Nothing to see here'
+    _description = "Backend Dummy Model demo"
 
     my_value = fields.Char(name="Value", required=True)
     my_other_value = fields.Char(name="Other value", required=True)
-    date_start = fields.Datetime(name="Date start", required=True, default=fields.Datetime.now)
-    date_stop = fields.Datetime(name="Date stop", required=True, default=fields.Datetime.now)
+    date_start = fields.Datetime(
+        name="Date start", required=True, default=fields.Datetime.now
+    )
+    date_stop = fields.Datetime(
+        name="Date stop", required=True, default=fields.Datetime.now
+    )
