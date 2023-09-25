5e7342a91c2a16b09c9517b6f5eb937ac86de526
Holger Brunn <mail@hunki-enterprises.com>
2023-09-25 13:00:21 +0200

    [MIG] base_user_role_company

--- base_user_role_company/__manifest__.py
+++ base_user_role_company/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "User roles by company",
-    "version": "16.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Tools",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "license": "AGPL-3",
