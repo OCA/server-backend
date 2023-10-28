10d9d100b3fc4ca97615aacac54ba735e17d4736
Enric Tobella <etobella@creublanca.es>
2023-10-28 15:00:44 +0200

    [FIX] base_user_role_company: Pylint fix

--- base_user_role_company/models/role.py
+++ base_user_role_company/models/role.py
@@ -26,9 +26,10 @@ class ResUsersRoleLine(models.Model):
                 and record.company_id not in record.user_id.company_ids
             ):
                 raise ValidationError(
-                    _('User "{}" does not have access to the company "{}"').format(
-                        record.user_id.name, record.company_id.name
+                    _(
+                        'User "%(user)s" does not have access to the company "%(company)s"'
                     )
+                    % {"user": record.user_id.name, "company": record.company_id.name}
                 )
 
     _sql_constraints = [
