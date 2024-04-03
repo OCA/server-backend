c46a53babf77ee1d212b8dc7993ed7dd25c20346
Ronald Portier <ronald@therp.nl>
2024-04-03 15:54:44 +0200

    [MIG] base_external_system_oath: migration to 16.0

--- base_external_system_oauth/__manifest__.py
+++ base_external_system_oauth/__manifest__.py
@@ -1,16 +1,15 @@
-# Copyright 2023 Therp BV <https://therp.nl>.
+# Copyright 2023-2024 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Base External System - oauth systems",
-    "summary": "Connect to external systems trough oauth authentication.",
-    "version": "10.0.1.0.0",
+    "summary": "Connect to external systems through oauth authentication.",
+    "version": "16.0.1.0.0",
     "category": "Base",
-    "website": "https://github.com/OCA/server-tools",
+    "website": "https://github.com/OCA/server-backend",
     "author": "Therp BV, Odoo Community Association (OCA)",
+    "maintainers": ["NL66278"],
     "license": "AGPL-3",
-    "application": False,
-    "installable": True,
     "depends": [
         "auth_oauth",
         "base_external_system_http",
@@ -19,4 +18,6 @@
         "views/external_system.xml",
         "views/auth_oauth_provider.xml",
     ],
+    "application": False,
+    "installable": True,
 }
