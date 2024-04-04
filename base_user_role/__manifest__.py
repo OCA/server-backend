576f4aa07b56e341f9aad01b34f3e562c73808a1
Dawn Hwang <hwangh95@gmail.com>
2024-04-04 10:38:41 +0200

    [FIX] base_user_role: Set group on access_wizard_groups_into_role
    From 17.0 onwards, any access right rule with set permissions must have
    a group on it, otherwise it will throw a warning on install

--- base_user_role/__manifest__.py
+++ base_user_role/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "User roles",
-    "version": "17.0.1.0.1",
+    "version": "17.0.1.0.2",
     "category": "Tools",
     "author": "ABF OSIELL, Odoo Community Association (OCA)",
     "license": "LGPL-3",
