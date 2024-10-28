25f4f20e5e7300dbb2f3f5b82360bf3164aa9587
David Beal <david.beal@akretion.com>
2024-10-28 22:26:28 +0100

    [ADD] polars_db_process: get db query in polars dataframe

---
+++ polars_db_process/__manifest__.py
@@ -0,0 +1,35 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Polars Database Process",
+    "version": "18.0.1.0.0",
+    "summary": "Allow to create a Polars dataframe from db.query and "
+    "check it and process it according to rules",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "development_status": "Alpha",
+    "website": "https://github.com/OCA/reporting-engine",
+    "maintainers": ["bealdav"],
+    "depends": [
+        "polars_process",
+    ],
+    "external_dependencies": {
+        "python": [
+            "connectorx",
+        ]
+    },
+    "data": [
+        "security/ir.model.access.xml",
+        "wizards/df_process.xml",
+        "views/dataframe.xml",
+        "views/df_field.xml",
+        "views/df_source.xml",
+        "views/db_config.xml",
+        "data/demo.xml",
+    ],
+    "demo": [
+        "data/demo.xml",
+    ],
+    "installable": True,
+}
---
+++ polars_db_process/models/__init__.py
@@ -0,0 +1,2 @@
+from . import df_source
+from . import db_config
