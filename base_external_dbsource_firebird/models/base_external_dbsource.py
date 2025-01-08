e3f13d458ac033e39613e480b715f911457a6e54
Abraham Anes <abrahamanes@gmail.com>
2025-01-08 22:52:49 +0100

    [IMP] base_external_dbsource_firebird: pre-commit auto fixes

--- base_external_dbsource_firebird/models/base_external_dbsource.py
+++ base_external_dbsource_firebird/models/base_external_dbsource.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2011 Daniel Reis
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 import logging
 
-from odoo import api
-from odoo import models
+from odoo import api, models
 
 _logger = logging.getLogger(__name__)
 
@@ -14,23 +12,26 @@ try:
     from odoo.addons.base_external_dbsource.models import (
         base_external_dbsource,
     )
+
     CONNECTORS = base_external_dbsource.BaseExternalDbsource.CONNECTORS
     try:
         import fdb
-        CONNECTORS.append(('fdb', 'Firebird'))
+
+        CONNECTORS.append(("fdb", "Firebird"))
     except:
-        _logger.info('Firebird library not available. Please install "fdb" '
-                     'python package.')
+        _logger.info(
+            'Firebird library not available. Please install "fdb" ' "python package."
+        )
 except ImportError:
-    _logger.info('base_external_dbsource Odoo module not found.')
+    _logger.info("base_external_dbsource Odoo module not found.")
 
 
 class BaseExternalDbsource(models.Model):
-    """ It provides logic for connection to an Firebird data source. """
+    """It provides logic for connection to an Firebird data source."""
 
     _inherit = "base.external.dbsource"
 
-    PWD_STRING_FDB = 'Password=%s;'
+    PWD_STRING_FDB = "Password=%s;"
 
     @api.multi
     def connection_close_fdb(self, connection):
@@ -39,9 +40,9 @@ class BaseExternalDbsource(models.Model):
     @api.multi
     def connection_open_fdb(self):
         kwargs = {}
-        for option in self.conn_string_full.split(';'):
+        for option in self.conn_string_full.split(";"):
             try:
-                key, value = option.split('=')
+                key, value = option.split("=")
             except ValueError:
                 continue
             kwargs[key.lower()] = value
