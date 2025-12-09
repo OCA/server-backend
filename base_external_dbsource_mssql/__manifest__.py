ce9787fb7671826470a539e1b25c2640b2ed2cd3
Pieter Paulussen <pieterpaulussen@code-source.be>
2025-12-09 09:07:01 +0100

    [FIX] [18.0] base_external_dbsource_mssql: remove version pinn from external library

--- base_external_dbsource_mssql/__manifest__.py
+++ base_external_dbsource_mssql/__manifest__.py
@@ -11,7 +11,7 @@
     "website": "https://github.com/OCA/server-backend",
     "license": "LGPL-3",
     "depends": ["base_external_dbsource"],
-    "external_dependencies": {"python": ["pymssql<=2.2.5", "sqlalchemy"]},
+    "external_dependencies": {"python": ["pymssql", "sqlalchemy"]},
     "demo": ["demo/base_external_dbsource.xml"],
     "installable": True,
 }
