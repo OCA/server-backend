64ccc129b0196115a4204fad80f9dfd4a0a500eb
Florent THOMAS <mailinglist@tdeo.fr>
2025-12-18 23:15:06 +0100

    [FIX]base_external_dbsource_mssql: bookworm lib compatibility
    Forward port of https://github.com/OCA/server-backend/pull/323/files
    
    [FIX]base_external_dbsource_mssql: LINT

--- base_external_dbsource_mssql/__manifest__.py
+++ base_external_dbsource_mssql/__manifest__.py
@@ -11,7 +11,13 @@
     "website": "https://github.com/OCA/server-backend",
     "license": "LGPL-3",
     "depends": ["base_external_dbsource"],
-    "external_dependencies": {"python": ["pymssql<=2.2.5", "sqlalchemy"]},
+    "external_dependencies": {
+        "python": [
+            "pymssql<=2.2.5 ; python_version <= '3.10'",
+            "pymssql<=2.2.8 ; python_version > '3.10'",
+            "sqlalchemy",
+        ]
+    },
     "demo": ["demo/base_external_dbsource.xml"],
     "installable": True,
 }
