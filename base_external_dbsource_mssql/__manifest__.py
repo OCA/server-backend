9cdc17978c121b385acbdf8bfe536fd1fcd89359
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2026-02-01 15:30:09 -0600

    [MIG] base_external_dbsource_mssql: Migration to 19.0

--- base_external_dbsource_mssql/__manifest__.py
+++ base_external_dbsource_mssql/__manifest__.py
@@ -4,7 +4,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "External Database Source - MSSQL",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "category": "Tools",
     "author": "Daniel Reis, LasLabs, Odoo Community Association (OCA)",
     "maintainers": ["anddago78"],
@@ -13,8 +13,7 @@
     "depends": ["base_external_dbsource"],
     "external_dependencies": {
         "python": [
-            "pymssql<=2.2.5 ; python_version <= '3.10'",
-            "pymssql<=2.2.8 ; python_version > '3.10'",
+            "pymssql",
             "sqlalchemy",
         ]
     },
