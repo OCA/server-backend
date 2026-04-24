ec62a45768cae1268af492849927b49cee7d9cea
tendil <mejta.diman@gmail.com>
2026-04-24 14:25:26 +0000

    [FIX] base_dav: restore odoo RUNBOT log level name
    Radicale re-registers logging level 25 as NOTICE during module import.
    
    Odoo uses the same numeric level for RUNBOT logs and expects it to be
    displayed as INFO. Because of this conflict, the standard
    "modules loaded..." log line is emitted as NOTICE and OCA checklog fails
    the pipeline even though tests pass.
    
    Restore the odoo mapping for RUNBOT after base_dav imports so level 25 is
    displayed as INFO again.
    
    Task: 5250

--- base_dav/__init__.py
+++ base_dav/__init__.py
@@ -1,5 +1,17 @@
 # Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import logging
+
 from . import models
 from . import controllers
 from . import radicale
+
+
+def _restore_odoo_runbot_level_name():
+    """Restore odoo RUNBOT level display name after Radicale imports."""
+    runbot_level = getattr(logging, "RUNBOT", 25)
+    if logging.getLevelName(runbot_level) != "INFO":
+        logging.addLevelName(runbot_level, "INFO")
+
+
+_restore_odoo_runbot_level_name()
