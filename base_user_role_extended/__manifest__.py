7fe71d18579675059f90581ea1b9f71d1e90ee23
Anusha <anusha.pp@calin.co.in>
2026-06-15 09:32:16 +0200

    [ADD] base_user_role_extended

---
+++ base_user_role_extended/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Base User Role Extended",
+    "version": "18.0.1.0.0",
+    "summary": "Extends user roles with additional access control features",
+    "author": "CIT Services, Odoo Community Association (OCA)",
+    "company": "CIT Services",
+    "website": "https://github.com/OCA/server-backend",
+    "depends": [
+        "base_user_role",
+    ],
+    "data": [],
+    "installable": True,
+    "license": "LGPL-3",
+}
