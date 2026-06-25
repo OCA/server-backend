d1a17718778f43fcf6aca40d2a1a9a3a43ae9cd6
Aungkokolin1997 <lin@quartile.co>
2026-06-25 09:28:27 +0000

    [IMP] base_import_match: pre-commit auto fixes

--- base_import_match/__manifest__.py
+++ base_import_match/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "18.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/server-backend",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
