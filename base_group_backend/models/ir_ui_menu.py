8f25220cfd6f570a570873b3a22d0789afbd7dda
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2026-09-15 07:22:10 +0200

    [MIG] base_group_backend: Migration to 19.0
    In v19 the user groups is no longer in module_category_user_type
    It has been replaced by a computed "Role" field - Extended here to cover
     the 2 backend groups.
    
    Tests have been updated to use inline data if demo data is not available

--- base_group_backend/models/ir_ui_menu.py
+++ base_group_backend/models/ir_ui_menu.py
@@ -8,7 +8,6 @@ class IrUiMenu(models.Model):
     _inherit = "ir.ui.menu"
 
     @api.model
-    @api.returns("self")
     def get_user_roots(self):
         """Avoid to display root menus with no defined groups_id to Backend UI Users
         like 'spreadsheet_dashboard.spreadsheet_dashboard_menu_root'
@@ -17,6 +16,6 @@ class IrUiMenu(models.Model):
         """
         res = super().get_user_roots()
         if self.env.user.has_group("base_group_backend.group_backend_ui_users"):
-            return res.filtered(lambda m: m.groups_id)
+            return res.filtered(lambda m: m.group_ids)
         else:
             return res
--- base_group_backend/tests/test_module.py
+++ base_group_backend/tests/test_module.py
@@ -1,32 +1,82 @@
 from odoo import Command
-from odoo.tests.common import TransactionCase
+
+from odoo.addons.base.tests.common import TransactionCaseWithUserDemo
 
 
-class TestResUsers(TransactionCase):
+class TestResUsers(TransactionCaseWithUserDemo):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.base_group_backend = cls.env.ref("base_group_backend.base_group_backend")
-        cls.internal_user = cls.env.ref("base.user_demo")
-        cls.portal_user = cls.env.ref("base_group_backend.user_demo_external")
-        cls.portal_ui_user = cls.env.ref(
-            "base_group_backend.user_demo_external_with_ui"
+        cls.internal_user = cls.user_demo
+        cls.portal_user = cls.env.ref(
+            "base_group_backend.user_demo_external", raise_if_not_found=False
         )
-        cls.menu_no_group = cls.env.ref("base_group_backend.menu_root_no_group")
+        if not cls.portal_user:
+            cls.portal_user = cls._create_user(
+                name="Demo partner backend 1",
+                login="demo backend user 1",
+                groups=[cls.env.ref("base.group_portal").id],
+            )
+        cls.portal_ui_user = cls.env.ref(
+            "base_group_backend.user_demo_external_with_ui", raise_if_not_found=False
+        )
+        if not cls.portal_ui_user:
+            cls.portal_ui_user = cls._create_user(
+                name="Demo partner backend 2",
+                login="demo backend user 2",
+                groups=[cls.env.ref("base_group_backend.group_backend_ui_users").id],
+            )
+        cls.menu_no_group = cls.env.ref(
+            "base_group_backend.menu_root_no_group", raise_if_not_found=False
+        )
+        if not cls.menu_no_group:
+            action = cls.env["ir.actions.act_window"].create(
+                {
+                    "name": "No Group",
+                    "res_model": "res.partner",
+                    "view_mode": "list,form",
+                }
+            )
+            cls.menu_no_group = cls.env["ir.ui.menu"].create(
+                {"name": "No Group", "sequence": 100}
+            )
+            cls.env["ir.ui.menu"].create(
+                {
+                    "name": "No Group Child",
+                    "sequence": 100,
+                    "parent_id": cls.menu_no_group.id,
+                    "action": f"ir.actions.act_window,{action.id}",
+                }
+            )
+
+    @classmethod
+    def _create_user(cls, name, login, groups=None):
+        """Helper method to create a user with the specified login and groups."""
+        partner = cls.env["res.partner"].create({"name": name})
+        user = cls.env["res.users"].create(
+            {
+                "name": login,
+                "login": login,
+                "group_ids": [(6, 0, groups or [])],
+                "partner_id": partner.id,
+            }
+        )
+        return user
 
     def test_has_groups(self):
         self.assertTrue(self.internal_user.has_group("base.group_user"))
         self.assertFalse(self.portal_user.has_group("base.group_user"))
         self.assertTrue(self.portal_ui_user.has_group("base.group_user"))
         self.portal_user.write(
-            {"groups_id": [Command.set([self.base_group_backend.id])]}
+            {"group_ids": [Command.set([self.base_group_backend.id])]}
         )
         self.assertTrue(self.portal_user.has_group("base.group_user"))
 
     def test_share(self):
         self.assertTrue(self.portal_user.share)
         self.portal_user.write(
-            {"groups_id": [Command.set([self.base_group_backend.id])]}
+            {"group_ids": [Command.set([self.base_group_backend.id])]}
         )
         self.assertFalse(self.portal_user.share)
         self.assertFalse(self.portal_ui_user.share)
